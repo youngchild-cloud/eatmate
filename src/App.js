a71b4756831d751662521836aead4cbccbebb064
youngchild-cloud <rladudcks8017@gmail.com>
2026-01-14 16:47:16 +0900

    'setting'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/reset.css';
+import './assets/scss/common.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label >test</label>
+      <input type="text" placeholder='입력' className='input-normal'></input>
+      <p>asdfasd</p>
+    </>
   );
 }
 
