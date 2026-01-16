db5ed94f03766ac774e4e7fe1a4409bd6d6dfc02
youngchild-cloud <rladudcks8017@gmail.com>
2026-01-16 18:20:23 +0900

    자유게시판 상세(제작중)

--- src/pages/community/CommunityDetail.js
+++ src/pages/community/CommunityDetail.js
@@ -1,12 +1,95 @@
 import React from 'react';
+
+import './CommunityDetail.scss'
+// 헤더 콤포넌트
 import TitleCenter from '../../components/common/TitleCenter';
+//이미지
+import default_profile from '../../assets/images/default_profile.png';
+//아이콘
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import { faComment as faCommentRegular } from "@fortawesome/free-regular-svg-icons";
 
 function CommunityDetail(props) {
   return (
     <section className='community-detail'>
       <div className="inner">
         <TitleCenter title={'자유게시판'} />
-        {/* 영찬님 => vw 바꾸는거랑 위에 타이틀 추가한거 내일 저한테 물어봐주세요!!! */}
+
+        {/* 자유게시판 글 목록  */}
+        <div className='item'>
+          {/* 프로필 + 닉네임 + 등록시간 */}
+          <div className="comm-top">
+            {/* 프로필 사진 public */}
+            <div className="img-box">
+              <img src={`${process.env.PUBLIC_URL}/images/community/user01.png`} alt="프로필 사진" />
+            </div>
+            {/* 닉네임 + 등록시간 */}
+            <p className='txt-box'>
+              <span className="nick">피자광인</span>
+              |
+              <span className="upload-time">23:13</span>
+            </p>
+          </div>
+
+          {/* 게시물 제목 + 게시글 내용 */}
+          <div className="comm-mid">
+            <h3 className='comm-title'>나 피자 진짜 좋아하는데</h3>
+            <p className='comm-content'>지금 시간에 먹으면 오바야?</p>
+          </div>
+
+          {/* 하트 수 + 채팅 수 */}
+          <div className="comm-bot">
+            <span>
+              <FontAwesomeIcon icon={faHeartRegular} />
+              12
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faCommentRegular} />
+              12
+            </span>
+          </div>
+        </div>
+
+        {/* ------댓글 입력------- */}
+        <div className='comm-input'>
+          <div className="input-img">
+            <img src={default_profile} alt="기본프로필" />
+          </div>
+          <form className="input-form">
+            <input type="text" placeholder='댓글을 입력하세요' />
+            <button>입력</button>
+          </form>
+        </div>
+
+        {/* ------게시글 댓글------ */}
+        <ul className='comm-comment'>
+          <li>
+            <div className="comment-img">
+              <img src={`${process.env.PUBLIC_URL}/images/community/user02.png`} alt="프로필 사진" />
+            </div>
+
+            <div className="comment-content">
+              <span className='comment-nick'>룰루랄라</span>
+              <p className='comment-txt'>넘 늦었다 지금은 ㄴㄴ</p>
+              <span className="comment-time">10분전</span>
+            </div>
+          </li>
+
+          <li>
+            <div className="comment-img">
+              <img src={`${process.env.PUBLIC_URL}/images/community/user03.png`} alt="프로필 사진" />
+            </div>
+
+            <div className="comment-content">
+              <span className='comment-nick'>sdfas</span>
+              <p className='comment-txt'>asdfasdfasdf</p>
+              <span className="comment-time">11분전</span>
+            </div>
+          </li>
+
+        </ul>
+
       </div>
     </section>
   );
--- src/pages/community/CommunityList.js
+++ src/pages/community/CommunityList.js
@@ -55,7 +55,7 @@ function CommunityList(props) {
           </Link>
 
           {/* 자유게시판 글 목록  */}
-          <Link to="/community/detail/">
+          <Link to="/community/detail/" className='item'>
             {/* 프로필 + 닉네임 + 등록시간 */}
             <div className="comm-top">
               {/* 프로필 사진 public */}
@@ -72,8 +72,8 @@ function CommunityList(props) {
 
             {/* 게시물 제목 + 게시글 내용 */}
             <div className="comm-mid">
-              <h3 className='comm-title'>나 피자 진짜 좋아하는데</h3>
-              <p className='comm-content'>지금 시간에 먹으면 오바야?</p>
+              <h3 className='comm-title'>나 피자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데자 진짜 좋아하는데</h3>
+              <p className='comm-content'>지금 시간에 먹으면 오바야?지금 시간에 먹으면 오바야?지금 시간에 먹으면 오바야?지금 시간에 먹으면 오바야?지금 시간에 먹으면 오바야?지금 시간에 먹으면 오바야?</p>
             </div>
 
             {/* 하트 수 + 채팅 수 */}
@@ -90,7 +90,7 @@ function CommunityList(props) {
           </Link>
 
           {/* 자유게시판 글 목록  */}
-          <Link to="/community/detail/">
+          <Link to="/community/detail/" className='item'>
             {/* 프로필 + 닉네임 + 등록시간 */}
             <div className="comm-top">
               {/* 프로필 사진 public */}
