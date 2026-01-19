433ca3f5cbcdeca24c412bdc5cc5cbd888a19d09
youngchild-cloud <rladudcks8017@gmail.com>
2026-01-19 18:17:47 +0900

    404페이지 제작중

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import error from '../../assets/images/404error/error.png';
 
 const NotFound = () => {
   return (
-    <div>
+    <>
+      <section className='page404'>
+        <article className="inner">
 
-    </div>
+          <div>
+            <img src={error} alt="404error" />
+          </div>
+
+
+        </article>
+      </section>
+
+    </>
   );
 };
 
