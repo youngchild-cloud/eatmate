e2c15f678ad8c87468491f85beb443d4279e9abe
youngchild-cloud <rladudcks8017@gmail.com>
2026-01-20 16:24:08 +0900

    404error페이지 제작완료

--- src/pages/not-found/NotFound.js
+++ src/pages/not-found/NotFound.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import error from '../../assets/images/404error/error.png';
+import { Link } from 'react-router-dom';
+import './notfound.scss';
 
 const NotFound = () => {
   return (
@@ -7,8 +9,17 @@ const NotFound = () => {
       <section className='page404'>
         <article className="inner">
 
-          <div>
-            <img src={error} alt="404error" />
+          {/* 404페이지 전체박스 */}
+          <div className='error'>
+            <div className='error-img'>
+              <img src={error} alt="404error" />
+            </div>
+            
+            <h2>찾으시는 페이지가 없습니다.</h2>
+            <p>요청하신 페이지를 찾을 수 없습니다.</p>
+            <p>입력하신 경로가 정확한지 다시 한번 확인해 주시기 바랍니다.</p>
+
+            <Link to="/" title="메인페이지로 가기" >홈으로</Link>
           </div>
 
 
