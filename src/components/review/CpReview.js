e9f4593e34995c047e5f1ae25be8b7adbfc95cac
hrkim0413 <hrkim041396@gmail.com>
2026-01-28 11:30:05 +0900

    맛집 리뷰 목록 > 데이터로 수정 작업 중

--- src/components/review/CpReview.js
+++ src/components/review/CpReview.js
@@ -1,76 +1,49 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './CpReview.scss';
 import Rank5 from 'components/review/Rank5';
 
 const CpReview = () => {
+  const [reviewData, setReviewData] = useState([]);
+
+  const loadData = async () => {
+    try {
+      const res = await axios.get('http://localhost:9070/review');
+      setReviewData(res.data);
+    } catch (err) {
+      console.log('axios error:', err);
+      console.log('status:', err?.response?.status);
+      console.log('data:', err?.response?.data);
+    }
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <>
       <ul id="cp-review">
-        <li>
-          <Link to={'/review/detail'}>
-            <div className="img-box">
-              <img src={`${process.env.PUBLIC_URL}/images/review/review01.jpg`} alt="리뷰" />
-            </div>
-            <div className="txt-box">
-              <p className='text'>크림파스타 넘맛있다 진짜 여기가 내 인생맛집인 듯</p>
-              <p className='name'>더 페어링</p>
-              <p className='info'><span>양식</span> · <span>잠실</span> | <sapn>4시간 전</sapn></p>
-              <Rank5 num={'5'} />
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to={'/review/detail'}>
-            <div className="img-box">
-              <img src={`${process.env.PUBLIC_URL}/images/review/review01.jpg`} alt="리뷰" />
-            </div>
-            <div className="txt-box">
-              <p className='text'>크림파스타 넘맛있다 진짜 여기가 내 인생맛집인 듯</p>
-              <p className='name'>더 페어링</p>
-              <p className='info'><span>양식</span> · <span>잠실</span> | <sapn>4시간 전</sapn></p>
-              <Rank5 num={'4'} />
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to={'/review/detail'}>
-            <div className="img-box">
-              <img src={`${process.env.PUBLIC_URL}/images/review/review01.jpg`} alt="리뷰" />
-            </div>
-            <div className="txt-box">
-              <p className='text'>크림파스타 넘맛있다 진짜 여기가 내 인생맛집인 듯</p>
-              <p className='name'>더 페어링</p>
-              <p className='info'><span>양식</span> · <span>잠실</span> | <sapn>4시간 전</sapn></p>
-              <Rank5 num={'3'} />
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to={'/review/detail'}>
-            <div className="img-box">
-              <img src={`${process.env.PUBLIC_URL}/images/review/review01.jpg`} alt="리뷰" />
-            </div>
-            <div className="txt-box">
-              <p className='text'>크림파스타 넘맛있다 진짜 여기가 내 인생맛집인 듯</p>
-              <p className='name'>더 페어링</p>
-              <p className='info'><span>양식</span> · <span>잠실</span> | <sapn>4시간 전</sapn></p>
-              <Rank5 num={'2'} />
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to={'/review/detail'}>
-            <div className="img-box">
-              <img src={`${process.env.PUBLIC_URL}/images/review/review01.jpg`} alt="리뷰" />
-            </div>
-            <div className="txt-box">
-              <p className='text'>크림파스타 넘맛있다 진짜 여기가 내 인생맛집인 듯</p>
-              <p className='name'>더 페어링</p>
-              <p className='info'><span>양식</span> · <span>잠실</span> | <sapn>4시간 전</sapn></p>
-              <Rank5 num={'1'} />
-            </div>
-          </Link>
-        </li>
+        {
+          reviewData.map(item => (
+            <li>
+              <Link to={'/review/detail'}>
+                <div className="img-box">
+                  <img src={`${process.env.PUBLIC_URL}/images/review/${item.br_img}`} alt={`${item.br_rt_name} 사진`} />
+                </div>
+                <div className="txt-box">
+                  <p className='text'>{item.br_desc}</p>
+                  <p className='name'>{item.br_rt_name}</p>
+                  <p className='info'>
+                    <span>(카테고리)</span> · <span>(위치)</span> | <span>{item.br_date}</span>
+                  </p>
+                  <Rank5 num={item.br_rank} />
+                </div>
+              </Link>
+            </li>
+          ))
+        }
       </ul>
     </>
   );
