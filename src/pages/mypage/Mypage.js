2b2d8b77b51c493164daae5ff1639d22cfd2827c
hrkim0413 <hrkim041396@gmail.com>
2026-02-03 22:14:28 +0900

    마이페이지 > 퍼블 수정

--- src/pages/mypage/Mypage.js
+++ src/pages/mypage/Mypage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 
 import './mypage.scss';
 
 import TitleCenter from 'components/common/TitleCenter';
-import ButtonWide from 'components/common/ButtonWide';
 import Badge from 'components/common/Badge';
+import { useRequireLogin } from 'utils/useRequireLogin';
 
 import BookmarkImg from 'assets/images/mypage/bookmark.png';
 import MeetupImg from 'assets/images/mypage/meetup.png';
@@ -14,8 +14,6 @@ import WriteImg from 'assets/images/mypage/write.png';
 import CommentImg from 'assets/images/mypage/comment.png';
 import HeartImg from 'assets/images/mypage/heart.png';
 import LogoutImg from 'assets/images/mypage/logout.png';
-import { useRequireLogin } from 'utils/useRequireLogin';
-import axios from 'axios';
 
 const Mypage = () => {
   useRequireLogin(); // 페이지에 진입했을 때 로그인이 안되어 있다면 로그인 페이지로 이동
@@ -23,18 +21,16 @@ const Mypage = () => {
   const token = localStorage.getItem('token');
   const decoded = token ? jwtDecode(token) : '';
 
-  console.log(decoded)
-
-  const [user, setUser] = useState({
-    u_no: decoded.token_no,
-    u_pic: '',
-    u_nick: '',
-    u_badge: '',
-    u_desc: ''
-  });
+  const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    if (window.confirm('로그아웃 하시겠습니까?')) {
+      localStorage.removeItem('token');
+      alert('로그아웃 되었습니다. 메인 페이지로 이동합니다.');
+      navigate('/');
+    } else {
+      return;
+    }
   }
 
   return (
@@ -53,22 +49,17 @@ const Mypage = () => {
           </p>
         </div>
 
-        <div className='profile-edit'><Link to='/mypage/profile'><ButtonWide text={'프로필 수정'} /></Link></div>
-
+        <div className='profile-edit'><Link to='/mypage/profile'>프로필 수정</Link></div>
 
         <div className='line'>
-          <Link to='/mypage/bookmark'><img src={BookmarkImg} alt="북마크아이콘" /><span>저장한 맛집</span></Link>
-          <Link to='/mypage/meetup'><img src={MeetupImg} alt="맛집아이콘" /><span>맛집 탐방 신청내역</span></Link>
-          <Link to='/mypage/write'><img src={WriteImg} alt="글쓰기아이콘" /><span>작성한 게시글</span></Link>
-          <Link to='/mypage/like'><img src={HeartImg} alt="좋아요아이콘" /><span>내가 남긴 좋아요</span></Link>
-          <Link to='/mypage/comment'><img src={CommentImg} alt="댓글아이콘" /><span>내가 남긴 댓글</span></Link>
+          <Link to='/mypage/bookmark'><img src={BookmarkImg} alt="북마크 아이콘" /><span>저장한 맛집</span></Link>
+          <Link to='/mypage/meetup'><img src={MeetupImg} alt="맛집 아이콘" /><span>맛집 탐방 신청내역</span></Link>
+          <Link to='/mypage/write'><img src={WriteImg} alt="글쓰기 아이콘" /><span>작성한 게시글</span></Link>
+          <Link to='/mypage/like'><img src={HeartImg} alt="좋아요 아이콘" /><span>내가 남긴 좋아요</span></Link>
+          <Link to='/mypage/comment'><img src={CommentImg} alt="댓글 아이콘" /><span>내가 남긴 댓글</span></Link>
         </div>
 
-        <Link to='/review' className='logout' onClick={handleLogout}><img src={LogoutImg} alt="로그아웃아이콘" /><span>로그아웃</span></Link>
-
-
-
-
+        <button className='logout' onClick={handleLogout}><img src={LogoutImg} alt="로그아웃 아이콘" /><span>로그아웃</span></button>
       </div>
     </section>
   );
